cbbdf99d6d531f0ea7324fff11677814b34366e6
Chrisy <chrisyhaigh@gmail.com>
2023-05-16 23:36:37 +0100

    JS file complete to generate lotto numbers in order

--- lottery.js
+++ lottery.js
@@ -1,29 +1,45 @@
-const randomize = () => Math.floor(Math.random() * 58) + 1;
-const randomBonus = () => Math.floor(Math.random()  * 58) + 1;
+
+//first creat variables 
+const ballNumbers = document.querySelectorAll('.ball');
+const generator = document.getElementById('generate');
 const duplicate = [];
 
-const firstBall = randomize();
-const secondBall = randomize();
-const thirdBall = randomize();
-const fourthBall = randomize();
-const fifthBall = randomize();
-const sixthBall = randomize();
-const bonusBall = randomBonus();
+generator.addEventListener('click', function() {
+    let numbers = [];
+
+    // Generate an array of unique random numbers
+    while (numbers.length < 6) {
+        let randomize = Math.floor(Math.random() * 58) + 1;
+        if (!duplicate.includes(randomize)) {
+            numbers.push(randomize);
+            duplicate.push(randomize);
+        }
+    }
+
+    //The sort method put numbers in ascending order with 2 arguements//
+
+    numbers.sort((a, b) => a - b);
+
+    // Assign each ball a unique number
+    ballNumbers.forEach((ball, index) => ball.innerHTML = numbers[index]);
+});
+
 
 //FUNCTIONS TO.....///
 
 /*if (duplicate balls 1-6 duplicate.push());*/
 /* return balls in the ascending order*/
 
-//Create generate Button//
+//Create generate Button(event listener onclick)//
 //Create IF statement if ball number between then should be certain colour//
 //also create function where number cannot be called more than once except BONUS//
 //innerText of each element should display different number in ball//
 
-console.log(`The firstball out is ${firstBall}`);
-console.log(`The second ball out is ${secondBall}`);
-console.log(`The third ball out is ${thirdBall}`);
-console.log(`The fourth ball out is ${fourthBall}`);
-console.log(`The fifth ball out is ${fifthBall}`);
-console.log(`The final ball out is ${sixthBall}`);
-console.log(`And the bonus ball is ${bonusBall}`);
+/*const firstBall = randomize();
+const secondBall = randomize();
+const thirdBall = randomize();
+const fourthBall = randomize();
+const fifthBall = randomize();
+const sixthBall = randomize();
+const bonusBall = randomBonus(); */
+
