cffbce8e363e2110e770690393d3591fbf21e5fd
chrisyhaigh <118481705+chrisyhaigh@users.noreply.github.com>
2023-05-16 23:42:22 +0100

    Update lottery.js

--- lottery.js
+++ lottery.js
@@ -1,29 +1,26 @@
-const randomize = () => Math.floor(Math.random() * 58) + 1;
-const randomBonus = () => Math.floor(Math.random()  * 58) + 1;
+
+//first creat variables 
+const ballNumbers = document.querySelectorAll('.ball');
+const generator = document.getElementById('generate');
 const duplicate = [];
 
-const firstBall = randomize();
-const secondBall = randomize();
-const thirdBall = randomize();
-const fourthBall = randomize();
-const fifthBall = randomize();
-const sixthBall = randomize();
-const bonusBall = randomBonus();
+generator.addEventListener('click', function() {
+    let numbers = [];
 
-//FUNCTIONS TO.....///
+    // Generate an array of unique random numbers
+    while (numbers.length < 6) {
+        let randomize = Math.floor(Math.random() * 58) + 1;
+        if (!duplicate.includes(randomize)) {
+            numbers.push(randomize);
+            duplicate.push(randomize);
+        }
+    }
 
-/*if (duplicate balls 1-6 duplicate.push());*/
-/* return balls in the ascending order*/
+    //The sort method put numbers in ascending order with 2 arguements//
 
-//Create generate Button//
-//Create IF statement if ball number between then should be certain colour//
-//also create function where number cannot be called more than once except BONUS//
-//innerText of each element should display different number in ball//
+    numbers.sort((a, b) => a - b);
 
-console.log(`The firstball out is ${firstBall}`);
-console.log(`The second ball out is ${secondBall}`);
-console.log(`The third ball out is ${thirdBall}`);
-console.log(`The fourth ball out is ${fourthBall}`);
-console.log(`The fifth ball out is ${fifthBall}`);
-console.log(`The final ball out is ${sixthBall}`);
-console.log(`And the bonus ball is ${bonusBall}`);
+    // Assign each ball a unique number
+    ballNumbers.forEach((ball, index) => ball.innerHTML = numbers[index]);
+});
+
